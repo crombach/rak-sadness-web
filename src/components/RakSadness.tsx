5f8cc14c2b9d208e6e493eafe70e17b46ed852ec
Cullen Rombach <cullenrombach@gmail.com>
2023-11-10 09:56:04 -0800

    Add footer message, move toast to top, style tweaks

--- src/components/RakSadness.tsx
+++ src/components/RakSadness.tsx
@@ -1,5 +1,5 @@
 import { Button, Sheet } from "@mui/joy";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { RakMadnessScores } from "../types/RakMadnessScores";
 import getClasses from "../utils/getClasses";
 import FloatingLabelInput from "./floatingLabelInput/FloatingLabelInput";
@@ -15,6 +15,11 @@ export default function RakSadness() {
     const [isScoresLoading, setScoresLoading] = useState(false);
     const [isViewLoading/* , setViewLoading */] = useState(false);
     const [isExportLoading, setExportLoading] = useState(false);
+
+    // Set document title
+    useEffect(() => {
+        document.title = "Rak Sadness"
+    }, []);
 
     const [scores, setScores] = useState<RakMadnessScores>();
     const scoreSpreadsheet = useCallback(() => {
@@ -56,57 +61,60 @@ export default function RakSadness() {
 
     return (
         <Sheet className="home" variant="plain" color="neutral">
-            <div className="home__week-input">
-                <FloatingLabelInput
-                    label="Week"
-                    placeholder="Rak Madness week number"
-                    value={week}
-                    onChange={(event) => {
-                        setScores(null);
-                        const digitsRegex = /^[0-9\b]+$/;
-                        if (event.target.value === "" || digitsRegex.test(event.target.value)) {
-                            setWeek(event.target.value);
-                        }
-                    }}
-                />
-            </div>
-            <Button
-                className={`home__submit-button ${getClasses({
-                    "--loading-btn": isScoresLoading
-                })}`}
-                variant="solid"
-                onClick={scoreSpreadsheet}
-                disabled={!week}
-            >
-                Select Picks Spreadsheet
-            </Button>
-            <div className={`home__actions ${getClasses({
-                "--expanded": !!week && !!scores
-            })}`}>
+            <div className="home__content">
+                <div className="home__week-input">
+                    <FloatingLabelInput
+                        label="Week"
+                        placeholder="Rak Madness week number"
+                        value={week}
+                        onChange={(event) => {
+                            setScores(null);
+                            const digitsRegex = /^[0-9\b]+$/;
+                            if (event.target.value === "" || digitsRegex.test(event.target.value)) {
+                                setWeek(event.target.value);
+                            }
+                        }}
+                    />
+                </div>
                 <Button
-                    className={`home__actions-button ${getClasses({
-                        "--loading-btn": isViewLoading
+                    className={`home__submit-button ${getClasses({
+                        "--loading-btn": isScoresLoading
                     })}`}
-                    disabled
-                    // disabled={!week || !scores || isScoresLoading}
                     variant="solid"
-                    color="success"
-                    onClick={() => console.log("TODO: Implement View Results")}
+                    onClick={scoreSpreadsheet}
+                    disabled={!week}
                 >
-                    View Results
+                    Select Picks Spreadsheet
                 </Button>
-                <Button
-                    className={`home__actions-button ${getClasses({
-                        "--loading-btn": isExportLoading
-                    })}`}
-                    disabled={!week || !scores || isScoresLoading}
-                    variant="solid"
-                    color="danger"
-                    onClick={exportResults}
-                >
-                    Export Results
-                </Button>
+                <div className={`home__actions ${getClasses({
+                    "--expanded": !!week && !!scores
+                })}`}>
+                    <Button
+                        className={`home__actions-button ${getClasses({
+                            "--loading-btn": isViewLoading
+                        })}`}
+                        disabled
+                        // disabled={!week || !scores || isScoresLoading}
+                        variant="solid"
+                        color="success"
+                        onClick={() => console.log("TODO: Implement View Results")}
+                    >
+                        View Results
+                    </Button>
+                    <Button
+                        className={`home__actions-button ${getClasses({
+                            "--loading-btn": isExportLoading
+                        })}`}
+                        disabled={!week || !scores || isScoresLoading}
+                        variant="solid"
+                        color="danger"
+                        onClick={exportResults}
+                    >
+                        Export Results
+                    </Button>
+                </div>
             </div>
+            <a className="home__footer" href="https://give.translifeline.org/give/461718/#!/donation/checkout" target="_blank">Trans rights are human rights 🏳️‍⚧️</a>
         </Sheet>
     );
 
