062026bfa1f5c19a2bcb8dc5d4541cebe8fa488c
Cullen Rombach <cullenrombach@gmail.com>
2024-09-24 13:49:02 -0700

    Make week number a dropdown

--- src/components/RakSadness.tsx
+++ src/components/RakSadness.tsx
@@ -1,11 +1,13 @@
-import { Button, Sheet } from "@mui/joy";
+import { Button, Select, Option, Sheet } from "@mui/joy";
 import {
   ChangeEventHandler,
   useCallback,
   useRef,
   useState,
   useEffect,
-  ChangeEvent,
+  MouseEvent,
+  KeyboardEvent,
+  FocusEvent,
   useMemo,
 } from "react";
 import { Toast, useToastContext } from "../context/ToastContext";
@@ -16,7 +18,6 @@ import buildSpreadsheetBuffer from "../utils/buildSpreadsheetBuffer";
 import getClasses from "../utils/getClasses";
 import { getPlayerScores, readFileToBuffer } from "../utils/getPlayerScores";
 import getCurrentWeekInfo from "../utils/getCurrentWeekInfo";
-import FloatingLabelInput from "./floatingLabelInput/FloatingLabelInput";
 import ScoresTable from "./table/scores/ScoresTable";
 import ExplanationTable from "./table/explanation/ExplanationTable";
 import "./RakSadness.css";
@@ -28,7 +29,7 @@ export default function RakSadness() {
   const { showToast } = useToastContext();
 
   // Loading flags
-  const [isWeekLoading, setWeekLoading] = useState(true);
+  const [isCurrentWeekLoading, setCurrentWeekLoading] = useState(true);
   const [isScoresLoading, setScoresLoading] = useState(true);
   const [isExportLoading, setExportLoading] = useState(false);
 
@@ -44,16 +45,17 @@ export default function RakSadness() {
     "Scoreboard" | "Explanation" | false
   >(false);
 
-  // Selected week
-  const [week, setWeek] = useState<string>("");
+  // Week state
+  const [currentWeek, setCurrentWeek] = useState<number>();
+  const [selectedWeek, setSelectedWeek] = useState<number>();
 
   // Query the ESPN API to get the current NFL week
   useEffect(() => {
     const getWeekAsync = async () => {
-      const currentWeek =
-        (await getCurrentWeekInfo(League.PRO))?.value?.toString() || "";
-      setWeek(currentWeek);
-      setWeekLoading(false);
+      const week = (await getCurrentWeekInfo(League.PRO))?.value;
+      setCurrentWeek(week);
+      setSelectedWeek(week);
+      setCurrentWeekLoading(false);
     };
     getWeekAsync();
   }, []);
@@ -61,18 +63,21 @@ export default function RakSadness() {
   // When the week changes, attempt to fetch the picks spreadsheet from the API.
   useEffect(() => {
     const fetchPicksAsync = async () => {
-      if (week && !isWeekLoading) {
+      if (selectedWeek && !isCurrentWeekLoading) {
         setScoresLoading(true);
         try {
-          const response = await fetch(`/api/picks/${week}`);
+          const response = await fetch(`/api/picks/${selectedWeek}`);
           response.headers;
           const picksBuffer = await response.arrayBuffer();
-          const newScores = await getPlayerScores(Number(week), picksBuffer);
+          const newScores = await getPlayerScores(
+            Number(selectedWeek),
+            picksBuffer,
+          );
           setScores(newScores);
         } catch (error) {
           // If the picks spreadsheet doesn't exist yet, fail gracefully and log a message.
           console.warn(
-            `Failed to load week ${week} picks spreadsheet from API. Has it been uploaded yet?`,
+            `Failed to load week ${selectedWeek} picks spreadsheet from API. Has it been uploaded yet?`,
             error,
           );
           setScores(null);
@@ -80,7 +85,7 @@ export default function RakSadness() {
             new Toast(
               "warning",
               "Missing Picks",
-              `Picks sheet for week ${week} is not yet in the database, but you can upload it manually.`,
+              `Picks sheet for week ${selectedWeek} is not yet in the database, but you can upload it manually.`,
             ),
           );
         } finally {
@@ -89,25 +94,19 @@ export default function RakSadness() {
       }
     };
     fetchPicksAsync();
-  }, [week, isWeekLoading]);
+  }, [selectedWeek, isCurrentWeekLoading]);
 
   // Whenever the week input value changes, update the component state.
-  const handleWeekInputChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      setScores(null);
-      setScoresLoading(true);
-      const digitsRegex = /^[0-9\b]+$/;
-      if (event.target.value === "" || digitsRegex.test(event.target.value)) {
-        setWeek(event.target.value);
-      }
-    },
-    [],
-  );
+  const handleWeekChange = useCallback((value: number) => {
+    setScores(null);
+    setScoresLoading(true);
+    setSelectedWeek(value);
+  }, []);
 
   // When a user manually uploads a picks spreadsheet, parse and score it.
   const handleFileUpload: ChangeEventHandler<HTMLInputElement> = useCallback(
     (event) => {
-      if (!week) {
+      if (!selectedWeek) {
         return;
       }
 
@@ -130,7 +129,10 @@ export default function RakSadness() {
         }
 
         const picksBuffer = await readFileToBuffer(files[0]);
-        const newScores = await getPlayerScores(Number(week), picksBuffer);
+        const newScores = await getPlayerScores(
+          Number(selectedWeek),
+          picksBuffer,
+        );
         if (newScores) {
           setScores(newScores);
           setScoresLoading(false);
@@ -148,19 +150,19 @@ export default function RakSadness() {
 
       scoreSpreadsheetAsync();
     },
-    [week],
+    [selectedWeek],
   );
 
   // Export the current scores to an Excel spreadsheet file.
   const exportResults = useCallback(() => {
-    if (!week) return;
+    if (!selectedWeek) return;
     const exportResultsAsync = async () => {
       setExportLoading(true);
 
       // Build the spreadsheet buffer.
       const spreadsheetBuffer = await buildSpreadsheetBuffer(
         scores,
-        Number(week),
+        Number(selectedWeek),
       );
 
       // Download the spreadsheet to the user's computer.
@@ -169,7 +171,7 @@ export default function RakSadness() {
       });
       const link = document.createElement("a");
       link.href = window.URL.createObjectURL(blob);
-      link.download = `rak-madness_week-${week}_scores.xlsx`;
+      link.download = `rak-madness_week-${selectedWeek}_scores.xlsx`;
       link.click();
       link.remove();
 
@@ -180,7 +182,7 @@ export default function RakSadness() {
       );
     };
     exportResultsAsync();
-  }, [scores, week]);
+  }, [scores, selectedWeek]);
 
   const navbarLeft = useMemo(() => {
     <>
@@ -259,20 +261,26 @@ export default function RakSadness() {
         color="neutral"
       >
         {/* Home Page */}
-        {!showScores && !isWeekLoading && (
+        {!showScores && !isCurrentWeekLoading && (
           <>
             {/* Input Controls */}
             <div className="home__controls">
               {/* Week number input */}
-              <div className="home__week-input">
-                <FloatingLabelInput
-                  label="Week"
-                  placeholder="Rak Madness week number"
-                  disabled={isWeekLoading}
-                  value={week}
-                  onChange={handleWeekInputChange}
-                />
-              </div>
+              <Select
+                className="home__week-input"
+                placeholder="Select a week..."
+                value={selectedWeek}
+                onChange={(_, value) => handleWeekChange(value)}
+                disabled={isCurrentWeekLoading}
+              >
+                {Array.from({ length: currentWeek }, (_, i) => i + 1).map(
+                  (weekNumber) => {
+                    return (
+                      <Option key={weekNumber} value={weekNumber}>Week {weekNumber}</Option>
+                    );
+                  },
+                )}
+              </Select>
               {/* Hidden picks file input */}
               <input
                 ref={fileInputRef}
@@ -284,12 +292,14 @@ export default function RakSadness() {
               {/* Upload picks file button */}
               <Button
                 className={`home__button ${getClasses({
-                  "--hide": isWeekLoading || isScoresLoading || !!scores,
+                  "--hide": isCurrentWeekLoading || isScoresLoading || !!scores,
                 })}`}
                 variant="solid"
                 color="primary"
                 onClick={clickFileInput}
-                disabled={!week || isWeekLoading || isScoresLoading}
+                disabled={
+                  !selectedWeek || isCurrentWeekLoading || isScoresLoading
+                }
               >
                 Upload Picks Spreadsheet
               </Button>
@@ -298,7 +308,12 @@ export default function RakSadness() {
                 className={`home__button --scores ${getClasses({
                   "--loading-btn": isScoresLoading,
                 })}`}
-                disabled={!week || isWeekLoading || !scores || isScoresLoading}
+                disabled={
+                  !selectedWeek ||
+                  isCurrentWeekLoading ||
+                  !scores ||
+                  isScoresLoading
+                }
                 variant="solid"
                 color="success"
                 onClick={() => setShowScores("Scoreboard")}
@@ -310,7 +325,12 @@ export default function RakSadness() {
                 className={`home__button --export ${getClasses({
                   "--loading-btn": isScoresLoading || isExportLoading,
                 })}`}
-                disabled={!week || isWeekLoading || !scores || isScoresLoading}
+                disabled={
+                  !selectedWeek ||
+                  isCurrentWeekLoading ||
+                  !scores ||
+                  isScoresLoading
+                }
                 variant="solid"
                 color="danger"
                 onClick={exportResults}
